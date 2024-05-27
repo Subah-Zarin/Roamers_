132234944adf621e299dd689aa307ff34589ed9c
Quazi Zarin Subah <155545263+Subah-Zarin@users.noreply.github.com>
2024-05-28 01:48:35 +0600

    started home Page

---
+++ lib/homepage/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget{
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body:  Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.teal,
+
+        ),
+      ),
+
+    );
+  }
+
+}
