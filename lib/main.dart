a371688a68406a1889429e407868a821d49b7012
Quazi Zarin Subah <155545263+Subah-Zarin@users.noreply.github.com>
2024-06-14 21:29:35 +0600

    started chat in another branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:roamers/homepage/homepage.dart';
 import 'package:roamers/login/login.dart';
 import 'package:roamers/login/registration.dart';
 import 'package:roamers/welcomePage/welcomepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: 'welcome',
     routes: {
-      'welcome' : (context) => welcomepage(),
-      'login' : (context) => login(),
-      'register' : (context) => registration(),
-      'homepage' : (context) => homepage(),
+      'welcome': (context) => welcomepage(),
+      'login': (context) => login(),
+      'register': (context) => registration(),
+      'homepage': (context) => homepage(),
     },
-
   ));
 }
