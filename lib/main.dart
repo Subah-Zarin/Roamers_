967e31ab660be2d43d1c66e274aa08d6125c039b
Quazi Zarin Subah <155545263+Subah-Zarin@users.noreply.github.com>
2024-06-19 21:49:48 +0600

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:roamers/firebase_options.dart';
 import 'package:roamers/homepage/homepage.dart';
 import 'package:roamers/login/login.dart';
 import 'package:roamers/login/registration.dart';
@@ -8,6 +10,10 @@ import 'package:roamers/welcomePage/welcomepage.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    name: 'name here',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
