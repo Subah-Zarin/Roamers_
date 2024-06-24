98b45d4bb5b8e0ac3680204354ca0fa56253a9ea
Quazi Zarin Subah <155545263+Subah-Zarin@users.noreply.github.com>
2024-06-24 18:44:04 +0600

    edited homepage

--- lib/widget/recommended_places.dart
+++ lib/widget/recommended_places.dart
@@ -11,94 +11,102 @@ class RecommendedPlaces extends StatelessWidget {
     return SizedBox(
       height: 235,
       child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return SizedBox(
-              width: 220,
-              child: Card(
-                elevation: 0.4,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
-                  ),
-                ),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(12),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TouristDetailsPage(
-                            image: recommendedPlaces[index].image,
-                          ),
-                        ));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(12),
-                          child: Image.asset(
-                            recommendedPlaces[index].image,
-                            width: double.maxFinite,
-                            fit: BoxFit.cover,
-                            height: 150,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Row(
-                          children: [
-                            const Text(
-                              "Bangladesh",
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const Spacer(),
-                            Icon(
-                              Icons.star,
-                              color: Colors.yellow.shade700,
-                              size: 14,
-                            ),
-                            const Text(
-                              "4.4",
-                              style: TextStyle(
-                                fontSize: 12,
-                              ),
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-                        Row(
-                          children: [
-                            Icon(
-                              Ionicons.location,
-                              color: Theme.of(context).primaryColor,
-                              size: 16,
-                            ),
-                            const SizedBox(width: 5),
-                            const Text(
-                              "Bangladesh",
-                              style: TextStyle(
-                                fontSize: 12,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return SizedBox(
+            width: 220,
+            child: Card(
+              elevation: 0.4,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
                 ),
               ),
-            );
-          },
-          separatorBuilder: (context, index) => const Padding(
-            padding: EdgeInsets.only(right: 10),
-          ),
-          itemCount: recommendedPlaces.length),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(12),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TouristDetailsPage(
+                        image: recommendedPlaces[index].image,
+                      ),
+                    ),
+                  );
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        recommendedPlaces[index].image,
+                        fit: BoxFit.cover,
+                        height: 150,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            recommendedPlaces[index].location,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow.shade700,
+                                size: 14,
+                              ),
+                              const SizedBox(width: 5),
+                              const Text(
+                                "4.4",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Icon(
+                                Ionicons.location,
+                                color: Theme.of(context).primaryColor,
+                                size: 16,
+                              ),
+                              const SizedBox(width: 5),
+                              Text(
+                                recommendedPlaces[index].location,
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(width: 10),
+        itemCount: recommendedPlaces.length,
+      ),
     );
   }
 }
